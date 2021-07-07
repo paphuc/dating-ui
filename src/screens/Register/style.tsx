aa5dd98f102402780978ccf80c69197c9217071b
nhath <minhnhat97kg@gmail.com>
2021-07-07 11:41:10 +0700

    UI login, register

---
+++ src/screens/Register/style.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native"
+export default StyleSheet.create({
+	Container: {
+		flex: 1,
+		flexDirection:"column",
+		justifyContent: "center",
+		alignItems:"stretch" ,
+	},
+	FormContainer: {
+		margin:30,
+		padding:20,
+		borderRadius:10,
+		backgroundColor:"white",
+	},
+
+})
