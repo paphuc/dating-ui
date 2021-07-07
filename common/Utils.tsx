e8c904f4067468e4e6e0c24a4af0dc103c87a488
nhath <minhnhat97kg@gmail.com>
2021-07-07 11:41:10 +0700

    UI login, register

--- common/Utils.tsx
+++
@@ -1,9 +0,0 @@
-export function clearLocalStorage() {
-	["access_token", "refresh_token", "user_info", "user_id"].forEach((item) => {
-		localStorage.removeItem(item)
-	})
-}
-
-export function getToken(tokenType : string) {
-	return localStorage.getItem(tokenType)
-}
