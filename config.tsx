b76ba7742958c9e9fa9e7bb8eb4c2d37c8557e00
nhath <minhnhat97kg@gmail.com>
2021-07-22 17:35:32 +0700

    update host

--- config.tsx
+++ config.tsx
@@ -1,6 +1,6 @@
 import { IConfig } from "./src/interfaces";
 let config: IConfig = {
-  Host: process.env.REACT_APP_HOST || "http://192.168.1.3",
-  Port: process.env.REACT_APP_PORT || "8080",
+  Host: process.env.REACT_APP_HOST || "https://dating-tma.herokuapp.com",
+  Port: process.env.REACT_APP_PORT || "443",
 };
 export default config;
