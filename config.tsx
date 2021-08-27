b4a9e11db558d4d2444df248aa161454190b7b85
tphuc <nguoigiaumat100@gmail.com>
2021-08-27 17:40:17 +0700

    commit fix

--- config.tsx
+++ config.tsx
@@ -1,8 +1,11 @@
 import { IConfig } from './src/interfaces'
 let config: IConfig = {
-  Host: process.env.REACT_APP_HOST || 'https://dating-tma.herokuapp.com',
-  Port: process.env.REACT_APP_PORT || '443',
-  WS: process.env.REACT_APP_WS || 'wss://dating-tma.herokuapp.com',
+  // Host: process.env.REACT_APP_HOST || 'https://dating-tma.herokuapp.com',
+  // Port: process.env.REACT_APP_PORT || '443',
+  // WS: process.env.REACT_APP_WS || 'wss://dating-tma.herokuapp.com',
+  Host: process.env.REACT_APP_HOST || 'http://192.168.1.172',
+  Port: process.env.REACT_APP_PORT || '8080',
+  WS: process.env.REACT_APP_WS || 'ws://192.168.1.172',
   BigRoomID: process.env.REACT_APP_ROOMID || '000000000000000000000000',
 }
 export default config
